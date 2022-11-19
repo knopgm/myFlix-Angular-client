8e914aae54014b57feee08db3478a8e8901b648e
knopgm <knop.gm@gmail.com>
2022-11-19 18:09:50 +0100

    Create api requsts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,19 +9,100 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://myflix-api-gkm.herokuapp.com';
 @Injectable({
   providedIn: 'root',
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {}
-  // Making the api call for the user registration endpoint
+
+  //1. Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http
       .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  //2. User login
+  public userLogin(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'login', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  //3. Get all movies enpoint
+  public getAllMovies(): Observable<any> {
+    return this.http.get(apiUrl + 'movies').pipe(catchError(this.handleError));
+  }
+
+  //4. Get one movie
+  public getOneMovie(title: string): Observable<any> {
+    console.log(title);
+    return this.http
+      .get(apiUrl + 'movies/' + title)
+      .pipe(catchError(this.handleError));
+  }
+
+  //5. Get director
+  public getDirector(name: string): Observable<any> {
+    console.log(name);
+    return this.http
+      .get(apiUrl + 'director/' + name)
+      .pipe(catchError(this.handleError));
+  }
+
+  //6. Get genre
+  public getGenre(title: string): Observable<any> {
+    console.log(title);
+    return this.http
+      .get(apiUrl + 'movies/genre/' + title)
+      .pipe(catchError(this.handleError));
+  }
+
+  //7. Add movie to favorite Movies
+  public addFavoriteMovie(username: string, movieID: string): Observable<any> {
+    console.log(username, movieID);
+    return this.http
+      .post(apiUrl + `users/${username}/movies/${movieID}`, {})
+      .pipe(catchError(this.handleError));
+  }
+
+  //8. Update user info
+  public updateUser(name: string, userDetails: any): Observable<any> {
+    console.log(name, userDetails);
+    return this.http
+      .put(apiUrl + 'users/' + name, userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  //9. Delete user
+  public deleteUser(name: string): Observable<any> {
+    console.log(name);
+    return this.http
+      .delete(apiUrl + 'users/' + name)
+      .pipe(catchError(this.handleError));
+  }
+
+  //10. Remove movie from favorite movies list
+  public removeFavoriteMovie(
+    username: string,
+    movieID: string
+  ): Observable<any> {
+    console.log(username, movieID);
+    return this.http
+      .delete(apiUrl + `users/${username}/movies/${movieID}`, {})
+      .pipe(catchError(this.handleError));
+  }
+
+  //11. Get user and get favorite movies of a user
+  public getUser(name: string): Observable<any> {
+    console.log(name);
+    return this.http
+      .get(apiUrl + `users/` + name)
       .pipe(catchError(this.handleError));
   }
 
@@ -35,8 +116,9 @@ export class UserRegistrationService {
     }
     return throwError('Something bad happened; please try again later.');
   }
-}
-
-export class FetchApiDataService {
-  constructor() {}
+  // Non-typed response extraction
+  private extractResponseData(res: any): any {
+    const body = res;
+    return body || {};
+  }
 }
